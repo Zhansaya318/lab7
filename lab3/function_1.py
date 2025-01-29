b2013383eeb75e455f8fd35b796bda70625fc062
beq <erlepesbek@gmail.com>
2025-01-29 15:42:55 +0500

    lab3

--- lab3/function_1.py
+++ lab3/function_1.py
@@ -2,14 +2,12 @@
 def to_ounces(grams):
     return 28.3495231 * grams
 
-print(to_ounces(3))
 
 
 #2
 def to_centigrade(F):
     return (5 / 9) * (F - 32)
 
-print(to_centigrade(3))
 
 #3
 def solve(numheads, numlegs):
@@ -18,7 +16,6 @@ def solve(numheads, numlegs):
             return i, numheads - i
     return 0, 0
 
-print(solve(35, 94))
 
 #4
 def isprime(n):
@@ -28,12 +25,6 @@ def isprime(n):
         if n % 2 == 0:
             return False
     return True
-
-numlist = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
-
-prime_numlist = list(filter(lambda x: isprime(x), numlist))
-
-print(prime_numlist)
 
 
 
@@ -46,29 +37,23 @@ def next_permutation(s):
         for x in next_permutation(s[:i]+s[i+1:]):
             new.append(s[i]+x)
     return new
-print(next_permutation("hello"))
+
 
 #6
-string = input("write: ")
-sentence = string.split()
-sentence.reverse()
-print(*sentence)
+def revstring(string):
+    sentence = string.split()
+    sentence.reverse()
+    return sentence
 
 #7
 def has_33(nums):
     for i in range(1, len(nums)):
         if(nums[i] == 3 and nums[i] == nums[i-1]):
-            print(True)
             return True
-    print(False)
     return False
 
-has_33([1, 3, 3]) 
-has_33([1, 3, 1, 3])
-has_33([3, 1, 3]) 
 
 
-print('\n')
 #8
 def spy_game(nums):
     numzero = 0
@@ -77,24 +62,17 @@ def spy_game(nums):
         if i == 0:
             numzero+=1
         elif numzero == 2 and i == 7:
-            print(True)
             return True
-    print(False)
     return False
     
 
-spy_game([1,2,4,0,0,7,5])
-spy_game([1,0,2,4,0,5,7])
-spy_game([1,7,2,0,4,5,0])
 
 
 #9
 import math
 def areaOfSphere(r):
-    print(4/3 *math.pi * r**3)
     return 4/3 *math.pi * r**3
 
-areaOfSphere(4)
 
 #10
 def uniquelist(arr):
@@ -103,22 +81,14 @@ def uniquelist(arr):
     for i in arr:
         if i not in unique_arr:
             unique_arr.append(i)
-    print(*unique_arr)
     return unique_arr
-
-uniquelist([1, 4, 5, 2, 5, 3, 1, 5, 8, 5, 3, 4, 5, 1, 7])
 
 #11
 def is_polindrom(s):
     revs = s[::-1]
     if revs == s:
-        print(True)
         return True
-    print(False)
     return False
-
-is_polindrom("Hello")
-is_polindrom("kazak")
 
 
 #12
@@ -126,20 +96,20 @@ def histogram(length):
     for i in length:
         print('*' * i)
 
-histogram([4, 9, 7])
 
 #13
 import random
-n = random.randint(1,21)
+def guess_num():
+    n = random.randint(1,21)
 
-print("Hello! What is your name?")
-name = input("")
-print(f"Well, {name}, I am thinking of a number between 1 and 20.\nTake a guess.")
-g = int(input())
-while g != n:
-    if g < n:
-        print("\nYour guess is too low.\nTake a guess.")
-    else:
-        print("\nYour guess is too low.\nTake a guess.")
+    print("Hello! What is your name?")
+    name = input("")
+    print(f"Well, {name}, I am thinking of a number between 1 and 20.\nTake a guess.")
     g = int(input())
-print(f"Good job, KBTU! You guessed my number in {n} guesses!")
+    while g != n:
+        if g < n:
+            print("\nYour guess is too low.\nTake a guess.")
+        else:
+            print("\nYour guess is too low.\nTake a guess.")
+        g = int(input())
+    print(f"Good job, KBTU! You guessed my number in {n} guesses!")
